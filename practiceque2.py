4436e1fbbfda69d205a82b4de2f1b6dfb0012dfc
Harashita28 <ashar28017@gmail.com>
2025-07-31 20:28:08 +0530

    Add files via upload

---
+++ practiceque2.py
@@ -0,0 +1,18 @@
+# r=10
+# while r>=1 :
+    
+#     print(r)
+#     r-=1
+# t = 2
+# while t<= 20:
+   
+#     print(t)
+#     t+=2
+i = 1
+sum = 0
+
+while i <= 15:
+    sum = sum + i
+    i += 1
+
+print("Sum is:", sum)
